29185b666ddf763e2de5d96b8e71ce8af2121d17
Donald551717 <okedonald.c@gmail.com>
2024-06-12 13:55:23 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+  int numbers[] = {2,4,6,7,9};
+    int sum = 0;
+    for(int number: numbers) {
+      sum += number;
+      
+    }
+    System.out.println(sum);
+  }
+
+}
