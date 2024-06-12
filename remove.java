9e4833f475e4778dfe6159b8df7b164f859fa130
Donald551717 <okedonald.c@gmail.com>
2024-06-12 15:29:20 +0100

    Update remove.java

--- remove.java
+++ remove.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     int numbers[] = {1, 2, 1, 3, 5, 1};
-    int key = 1;
-    int output[] = removeElement(numbers, key);
+    int remove = 1;
+    int output[] = removeElement(numbers, remove);
     System.out.println(Arrays.toString(output));
   }
-  public static int[] removeElement(int[] nums, int key) {
+  public static int[] removeElement(int[] nums, int remove) {
     int count = 0;
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != key) {
+      if (nums[i] != remove) {
         count++;
       }
     }
     int[] output = new int[count];
     int index = 0;
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != key) {
+      if (nums[i] != remove) {
         output[index] = nums[i];
         index++;
       }
